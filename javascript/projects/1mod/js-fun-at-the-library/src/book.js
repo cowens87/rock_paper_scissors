067dbde5a2e8b5f3623d247d7ac746964be0e214
Cydnee Owens <cks410@yahoo.com>
2022-04-16 09:33:04 -0700

    add books

--- javascript/projects/1mod/js-fun-at-the-library/src/book.js
+++ javascript/projects/1mod/js-fun-at-the-library/src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var bookCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+  return bookCharacter;
+}
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    return reviews.push(review);
+  }
+}
+
+function calculatePageCount(title) {
+  return (title.length * 20)
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var bookInfo = {
+    title: title,
+    mainCharacter: mainCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(title)
+  };
+  return bookInfo
+}
+
+function editBook(book) {
+  book.pageCount = (calculatePageCount(book.title) * 0.75)
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
