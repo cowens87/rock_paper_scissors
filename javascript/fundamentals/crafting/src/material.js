2c17ef9f0f5185ee2164a5dde9b29770c771290a
Cydnee Owens <cks410@yahoo.com>
2022-04-19 23:00:12 -0700

    Add mythical creatures

---
+++ javascript/fundamentals/crafting/src/material.js
@@ -0,0 +1,22 @@
+class Material {
+  constructor(name, price, amount, units) {
+    this.name   = name;
+    this.price  = price;
+    this.amount = amount;
+    this.units  = units;
+  }
+
+  useMaterial(qty) {
+    if (qty > this.amount) {
+      return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`;
+    } 
+    this.amount -= qty
+    return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+  }
+
+  calculateMaterialCost() {
+    return this.amount * this.price;
+  }
+}
+
+module.exports = Material;
---
+++ javascript/fundamentals/do-you-wanna-build-a-snowman/src/Human.js
@@ -0,0 +1,28 @@
+var Snowman = require('./Snowman');
+
+class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {snowballs: 0, coal: 0, buttons: 0, carrots: 0}
+  }
+
+  gatherMaterials(item, qty) {
+    this.materials[item] += qty;
+  }
+
+  buildASnowman() {
+    this.snowman = new Snowman(this.materials);
+    return this.snowman;
+  }
+
+  placeMagicHat() {
+    this.snowman.canWearMagicHat();
+    if (this.snowman.magicHat) {
+      return 'Woah, this snowman is coming to life!';
+    }
+    return 'I guess I didn\'t build it correctly.';
+  }
+}
+
+module.exports = Human;
