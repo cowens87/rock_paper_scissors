4a95087adc3afe26ea6859970bdc0e20491f67a8
Cydnee Owens <cks410@yahoo.com>
2022-04-20 17:05:14 -0700

    Add restaurant code

--- javascript/projects/1mod/js-fun-at-a-restaurant/src/restaurant.js
+++ javascript/projects/1mod/js-fun-at-a-restaurant/src/restaurant.js
@@ -1,7 +1,25 @@
+function createRestaurant(name) {
+  var info = { name: name, menus: { breakfast: [], lunch: [], dinner: [] }
+  };
+  return info;
+}
 
+function addMenuItem(restaurant, item) {
+  if(!restaurant.menus[item.type].includes(item)) {
+    return restaurant.menus[item.type].push(item);
+  }
+}
+
+function removeMenuItem(restaurant, name, type) {
+  if(restaurant.menus[type].map(item => item.name).includes(name)) {
+    restaurant.menus[type].splice(restaurant.menus[type].findIndex(i => i.name === name), 1);
+    return `No one is eating our ${name} - it has been removed from the ${type} menu!`;
+  };
+  return `Sorry, we don't sell ${name}, try adding a new recipe!`;
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem
 }
