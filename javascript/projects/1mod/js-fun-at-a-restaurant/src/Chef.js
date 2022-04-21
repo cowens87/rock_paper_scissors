0233e6d758fdd2cdcad40488c016df2df18a378c
Cydnee Owens <cks410@yahoo.com>
2022-04-20 17:45:52 -0700

    Add Chef code

--- javascript/projects/1mod/js-fun-at-a-restaurant/src/Chef.js
+++ javascript/projects/1mod/js-fun-at-a-restaurant/src/Chef.js
@@ -1,6 +1,19 @@
 class Chef {
-  constructor() {
-    
+  constructor(chefName, restaurant = {}) {
+    this.name = chefName;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(name, morning = false) {
+    if(morning === true) { return `Good morning, ${name}!`; }
+    return `Hello, ${name}!`;
+  }
+
+  checkForFood(item) {
+    if(this.restaurant.menus[item.type].includes(item)) {
+      return `Yes, we're serving ${item.name} today!`
+    }
+    return `Sorry, we aren't serving ${item.name} today.`;
   }
 }
 
